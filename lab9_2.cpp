53272e08ac33afbfd5c972883406f67a72b9956f
litnps <lalitth21.n@gmail.com>
2025-12-19 14:57:31 +0700

    Accepted : lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int n, int m){
+	if(n <= 0 || m <= 0){
+		cout<<"Invalid input\n";
+		return ;
+	}
+	
+	for(int i = 0 ; i < n ; ++i){
+		for(int j = 0 ;  j < m ; ++j){
+			cout<<"O";
+		}
+		cout<<"\n";
+	}
 
+	return;
+}
 int main(){
 	
 	printO(2,2);
